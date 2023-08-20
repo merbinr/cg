566afd87321d948ac290a727f2e2bc455f71d00b
Shibly Meeran <git@shib.me>
2023-08-20 22:02:46 +0530

    updating with more images and a shell script

---
+++ mirror_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+image_name=$1
+version=$2
+echo "Mirroring $image_name:$version"
+IFS="." read -r -a parts <<< $version
+versions=(latest)
+current=""
+for value in ${parts[@]}; do [[ -z $current ]] && current=$value || current+=".$value" ; versions=(${versions[@]} "$current"); done
+for tag in ${versions[@]}; do ./regctl image copy $SOURCE_REGISTRY/$image_name:latest $TARGET_REGISTRY/$image_name:$tag; done
