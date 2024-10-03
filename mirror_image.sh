c2068d2b39ddb509e864c0fc7b7be2b5bc3e7c57
Merbin Russel <reachme@merb.in>
2024-10-03 16:22:27 +0530

    Fixed debug image not pulling issue

--- mirror_image.sh
+++ mirror_image.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 image_name=$1
 version=$2
+source_tag=$3
+
 echo "Mirroring $image_name:$version"
 IFS="." read -r -a parts <<< $version
 versions=(latest)
 current=""
 for value in ${parts[@]}; do [[ -z $current ]] && current=$value || current+=".$value" ; versions=(${versions[@]} "$current"); done
-for tag in ${versions[@]}; do ./regctl image copy $SOURCE_REGISTRY/$image_name:latest $TARGET_REGISTRY/$image_name:$tag; done
+for tag in ${versions[@]}; do ./regctl image copy $SOURCE_REGISTRY/$image_name:"$source_tag" $TARGET_REGISTRY/$image_name:$tag; done
